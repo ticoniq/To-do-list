5889ec2678ec3b96af9910706995de0212ad848b
ticoniq_ <okolietochi@gmail>
2023-06-07 19:16:57 +0100

    Adding, display, delete, and updata items with locahstorage

--- src/index.js
+++ src/index.js
@@ -1,19 +1,44 @@
 import './styles/style.css';
-import todoData from './modules/data';
+import Todo from './modules/data';
+import UI from './modules/ui';
 
-const ul = document.querySelector('.ul');
+const ui = new UI();
+const form = document.querySelector('.form');
+const clearBtn = document.querySelector('.clearBtn');
 
-function displayTask() {
-  todoData.forEach((data) => {
-    const list = document.createElement('li');
-    list.innerHTML = `
-      <div><input type="checkbox" id="check">
-        <p>${data.description}</p>
-      </div>
-      <i class="fas fa-ellipsis-v"></i>
-    `;
-    ul.appendChild(list);
-  });
-}
+document.addEventListener('DOMContentLoaded', UI.displayFromLocalStorage);
 
-document.addEventListener('DOMContentLoaded', displayTask);
+// Add todo list
+form.addEventListener('submit', (e) => {
+  const desc = document.querySelector('.desc').value;
+  const newId = UI.getItem();
+  let index;
+  if (newId.length > 0) {
+    index = newId[newId.length - 1].index + 1;
+  } else {
+    index = 0;
+  }
+
+  const completed = false;
+
+  if (desc === '') {
+    ui.errorMsg('Error', 'rgba(255, 0, 0, 0.5)');
+  } else {
+    // Init todo data
+    const newTodo = new Todo(index, desc, completed);
+
+    // Display data on ui
+    ui.displayTask(newTodo);
+
+    UI.addToLocalStorage(newTodo);
+
+    ui.errorMsg('Success', 'rgba(9, 186, 9, 0.5)');
+
+    document.querySelector('.desc').value = '';
+  }
+
+  e.preventDefault();
+});
+
+// Clear all task
+clearBtn.addEventListener('click', UI.clearCompletedTasks);
